893c32fd35bc287aadc7f6d1b474a1c481c3dab2
Gisela <giselacepeda91@gmail.com>
2021-04-13 09:49:22 +0200

    first commit

---
+++ mixed-messages.js
@@ -0,0 +1,37 @@
+let yesButton =document.getElementById('yes');
+
+let noButton = document.getElementById('no');
+
+messages = ['You will know it when you see it. It will know you when it sees you.', 
+                  'From listening comes wisdom and from speaking repentance.', 
+                  'Courtesy begins in the home.',
+                  'Better ask twice than lose yourself once.',
+                  'An acquaintance of the past will affect you in the near future.',
+                  'A soft voice may be awfully persuasive.',
+                  'A hunch is creativity trying to tell you something.',
+                  'A dubious friend may be an enemy in camouflage.',
+                  'To be found, stop hiding.',
+                  'Some things are just too heavy to haul around.',
+                  'Change comes with embracing the future, not fighting your past.',
+                  'Unlearn everything that you aren’t. Relearn everything that you are.',
+                  'When you love yourself, people who do not love you back become less interesting.',
+                  'The middle of the process is no place to determine the end of it.',
+                  'Follow what you love and see what turns up.'
+            ]
+
+
+//console.log(messages)
+
+ 
+function fortuneMessage(event) {
+    let fortune = messages[Math.floor(Math.random() * 15)] ;
+    event.target.innerHTML.h2 = fortune
+}          
+
+
+function noFortune(event) {
+    event.target.innerHTML.h2 = 'See you next time!'
+}
+
+yesButton.addEventListener('click', fortuneMessage);
+noButton.addEventListener('click', noFortune)
